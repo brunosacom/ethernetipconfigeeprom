71002779f05705caa8f3f6b1163af6d334f3eeef
brunosacom <bruno.sa.com@gmail.com>
2024-09-24 21:30:09 -0300

    JQuery3.7.1

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Este sketch exibe uma página WEB para configuração dos dados de IP, subnet ma
 Esses dados são gravados na EEPROM do Arduino e assim mesmo ao desligar, as configurações irão permanecer.
 
 O código HTML já tem inseridos:
- - BootStrap 5.1.3
+ - BootStrap 5.3.3
  - Popper 2.9.1
- - JQuery 3.6.0
- - JqueryUI 1.12.1
+ - JQuery 3.7.1
+ - JqueryUI 1.14.0
  - JQueryUI Touch Punch 0.2.3
  - GoogleFonts
 
--- ethernetipconfigeeprom.ino
+++ ethernetipconfigeeprom.ino
@@ -195,7 +195,7 @@ void loop()
           client.println(F("<!-- Bootstrap CSS -->"));
           client.println(F("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>"));
           client.println(F("<!-- jQuery UI CSS -->"));
-          client.println(F("<link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>"));
+          client.println(F("<link rel='stylesheet' href='https://code.jquery.com/ui/1.14.0/themes/base/jquery-ui.css'>"));
 
           //BMBS internal page: /fsend
           //character conversion in https://www.arduino.cc/en/Reference/ASCIIchart
@@ -337,8 +337,8 @@ void loop()
           client.println(F("</div>"));
           client.println(F("<!-- Optional JavaScript -->"));
           client.println(F("<!-- jQuery first, then Popper.js, then Bootstrap JS -->"));
-          client.println(F("<script src='https://code.jquery.com/jquery-3.6.0.js' integrity='sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=' crossorigin='anonymous'></script>"));
-          client.println(F("<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js' integrity='sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=' crossorigin='anonymous'></script>"));
+          client.println(F("<script src='https://code.jquery.com/jquery-3.7.1.js' integrity='sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=' crossorigin='anonymous'></script>"));
+          client.println(F("<script src='https://code.jquery.com/ui/1.14.0/jquery-ui.js' integrity='sha256-u0L8aA6Ev3bY2HI4y0CAyr9H8FRWgX4hZ9+K7C2nzdc=' crossorigin='anonymous'></script>"));
           client.println(F("<script src='https://cdn.jsdelivr.net/npm/jquery-ui-touch-punch@0.2.3/jquery.ui.touch-punch.js' integrity='sha256-S9605h/+fTHx8kE89v4NQWuTGCEQJF0B9UGvMFYAiO8=' crossorigin='anonymous'></script>"));
           client.println(F("<script src='https://unpkg.com/@popperjs/core@2'></script>"));
           client.println(F("<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js' integrity='sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy' crossorigin='anonymous'></script>"));
